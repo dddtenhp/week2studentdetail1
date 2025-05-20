7d975c484ba82c1800bac5575d8111721cccc9af
hhtan <pham43@sheridancollege.ca>
2025-05-20 16:42:09 -0400

    ADD LINE3

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,10 +16,8 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");        
-        System.out.println("add line 2");        System.out.println("add line 2");
+        System.out.println("add line 2");       
         System.out.println("add line 3");
-
-
     }
     
 }
